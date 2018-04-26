bf66c1acbe526979d27505def8d8874ee4c6f2c9
Tom <ganleyapps@gmail.com>
2018-04-25 21:31:54 -0700

    Add a Photo object

---
+++ flickrViewer/Model/Photo.swift
@@ -0,0 +1,13 @@
+//
+//  Photo.swift
+//  flickrViewer
+//
+//  Created by Thomas Ganley on 4/25/18.
+//  Copyright © 2018 Thomas Ganley. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Decodable {
+    
+}
