f356b87228f56eb9ae625eb6eec33e63048b87ea
Tom <ganleyapps@gmail.com>
2018-04-25 21:26:01 -0700

    Add a constants file for storing API constants.

---
+++ flickrViewer/Common/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  flickrViewer
+//
+//  Created by Thomas Ganley on 4/25/18.
+//  Copyright © 2018 Thomas Ganley. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct FlickrAPI {
+        static let apiKey = "1508443e49213ff84d566777dc211f2a"
+        static let apiKeyKey = "api_key"
+        static let endpointURLString = "https://api.flickr.com/services/rest/"
+        static let textKey = "text"
+    }
+}
