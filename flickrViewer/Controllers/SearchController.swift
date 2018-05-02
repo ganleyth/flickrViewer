23ea68be2b3218b30baebdf4b899c1fd8f4ea7f3
Tom <ganleyapps@gmail.com>
2018-05-01 19:01:43 -0700

    Add a search controller with a signature for the main search method.

---
+++ flickrViewer/Controllers/SearchController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchController.swift
+//  flickrViewer
+//
+//  Created by Thomas Ganley on 5/1/18.
+//  Copyright © 2018 Thomas Ganley. All rights reserved.
+//
+
+import Foundation
+
+class SearchController {
+    
+    static func search(searchTerm: String, page: Int, completion: (_ photos: [Photo]?) -> Void) {
+    }
+}
