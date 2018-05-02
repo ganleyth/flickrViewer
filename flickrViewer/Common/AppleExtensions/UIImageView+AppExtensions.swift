9666b4e228c7eed5bf7a548d03343535fc455efd
Tom <ganleyapps@gmail.com>
2018-05-01 18:12:26 -0700

    Add an image loader

---
+++ flickrViewer/Common/AppleExtensions/UIImageView+AppExtensions.swift
@@ -0,0 +1,30 @@
+//
+//  UIImageView+AppExtensions.swift
+//  flickrViewer
+//
+//  Created by Thomas Ganley on 4/27/18.
+//  Copyright © 2018 Thomas Ganley. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    
+    func loadImage(at url: URL) {
+        self.image = #imageLiteral(resourceName: "defaultPhoto")
+        
+        NetworkController.get(url: url) { [weak self] (data, error) in
+            guard let this = self else { return }
+            if let error = error {
+                print("Error fetching image: \(error.localizedDescription)")
+                return
+            }
+            
+            guard
+                let data = data,
+                let image = UIImage(data: data) else { return }
+            
+            this.image = image
+        }
+    }
+}
